c47be7e906c443f60e699424b982aea6d0c1b68f
Furkan TOPTAŞ <toptas.furkan@gmail.com>
2022-03-04 22:21:23 +0300

    Create Project, First commit

---
+++ HomeworksOne/HomeworksOne/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeworksOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
