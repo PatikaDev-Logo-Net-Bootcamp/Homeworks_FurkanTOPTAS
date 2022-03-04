b1b818d230d6029d91c3b5f2897fa9c98f1289d2
Furkan TOPTAŞ <toptas.furkan@gmail.com>
2022-03-04 23:02:27 +0300

    Changed first screen

--- HomeworksOne/HomeworksOne/Startup.cs
+++ HomeworksOne/HomeworksOne/Startup.cs
@@ -50,7 +50,7 @@ namespace HomeworksOne
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
