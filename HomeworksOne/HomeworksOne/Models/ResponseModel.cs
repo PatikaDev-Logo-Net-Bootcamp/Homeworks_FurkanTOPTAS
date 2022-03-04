2099a658f7a357cf6bbf0df4b4d7b6b2e566c8fe
Furkan TOPTAŞ <toptas.furkan@gmail.com>
2022-03-04 22:55:03 +0300

    Clone ResponseModel from my private repo

---
+++ HomeworksOne/HomeworksOne/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeworksOne.Models
+{
+    public class ResponseModel
+    {
+        public bool Success { get; set; }
+        public string Data { get; set; }
+        public string Error { get; set; }
+    }
+}
