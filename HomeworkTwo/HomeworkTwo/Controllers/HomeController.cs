63b82fde64812d3e104a5e783b2e929fc0dbecd8
Furkan TOPTAŞ <toptas.furkan@gmail.com>
2022-03-14 22:41:36 +0300

    Created HomeController

---
+++ HomeworkTwo/HomeworkTwo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeworkTwo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+
+    }
+}
