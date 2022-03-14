5dbc590cbc4b446e96ab917b1e1b2ba50ed4f148
Furkan TOPTAŞ <toptas.furkan@gmail.com>
2022-03-14 23:19:56 +0300

    add version control middleware

--- HomeworkTwo/HomeworkTwo/Middlewares/AppVersionControlMiddleware.cs
+++ HomeworkTwo/HomeworkTwo/Middlewares/AppVersionControlMiddleware.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Threading.Tasks;
 
 namespace HomeworkTwo.Middlewares
@@ -8,17 +10,47 @@ namespace HomeworkTwo.Middlewares
     public class AppVersionControlMiddleware
     {
         private readonly RequestDelegate _next;
+        private IConfiguration _config;
+        
 
-        public AppVersionControlMiddleware(RequestDelegate next)
+        public AppVersionControlMiddleware(RequestDelegate next, IConfiguration config)
         {
+            _config = config;
             _next = next;
         }
 
         public Task Invoke(HttpContext httpContext)
         {
+            double versionHeader;
+            double versionDefault; // app settigns
 
-            return _next(httpContext);
+            if (httpContext.Request.Path=="/api/Home/Login/" || httpContext.Request.Path == "/api/Home/Register/")
+            {
+                return _next(httpContext);
+            }
+            else
+            {
+                double.TryParse(httpContext.Request.Headers["app-version"], out versionHeader); // Headerdan gelen data
+                double.TryParse(_config.GetValue<string>("Version"), out versionDefault); // app setting den gelen data 
+
+                if (versionHeader > versionDefault)
+                {
+                    return ErrorReturnException(httpContext,"Your version is bigger than my version");
+                }
+               
+                return _next(httpContext);
+            }
+
+            
+
         }
+
+        private async Task ErrorReturnException(HttpContext httpContext, String message)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await httpContext.Response.WriteAsync(message);
+        }
+   
     }
 
     // Extension method used to add the middleware to the HTTP request pipeline.
