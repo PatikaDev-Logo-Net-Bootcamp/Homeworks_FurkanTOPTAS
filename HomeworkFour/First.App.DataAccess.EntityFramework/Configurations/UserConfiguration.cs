8c90c2f7c3f0dc0690cce37794a26c9e66485d6a
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-03-29 22:34:42 +0300

    Add Table User for MSSQL

---
+++ HomeworkFour/First.App.DataAccess.EntityFramework/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using First.App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First.App.DataAccess.EntityFramework.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
