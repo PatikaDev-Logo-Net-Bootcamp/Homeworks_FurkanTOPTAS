eefef60cee1e44675f6177364ab646128b702a38
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-03-29 22:34:18 +0300

    Added UserServices

---
+++ HomeworkFour/First.App.Core/Concretes/UserServices.cs
@@ -0,0 +1,44 @@
+﻿using First.App.Business.Abstract;
+using First.App.DataAccess.EntityFramework.Repository.Abstracts;
+using First.App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace First.App.Business.Concretes
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> repository;
+        private readonly IUnitOfWork unitOfWork;
+        public UserService(IRepository<User> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+        public void AddUser(User user)
+        {
+            repository.Add(user);
+            unitOfWork.Commit();
+        }
+
+        public void DeleteUser(User user)
+        {
+            repository.Delete(user);
+            unitOfWork.Commit();
+        }
+
+        public List<User> GetAllUser()
+        {
+            return repository.Get().ToList();
+        }
+
+        public void UpdateUser(User user)
+        {
+
+            repository.Update(user);
+            unitOfWork.Commit();
+        }
+    }
+}
