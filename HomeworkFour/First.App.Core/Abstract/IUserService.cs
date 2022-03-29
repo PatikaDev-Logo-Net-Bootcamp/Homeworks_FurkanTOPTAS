3cd4159bacb8a8c6b453ae175b5f5f3a75aa3fe1
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-03-29 22:32:26 +0300

    Add User Methods

---
+++ HomeworkFour/First.App.Core/Abstract/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using First.App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First.App.Business.Abstract
+{
+    public interface IUserService
+    {
+        List<User> GetAllUser();
+        void AddUser(User user);
+        void DeleteUser(User user);
+        void UpdateUser(User user);
+
+    }
+}
