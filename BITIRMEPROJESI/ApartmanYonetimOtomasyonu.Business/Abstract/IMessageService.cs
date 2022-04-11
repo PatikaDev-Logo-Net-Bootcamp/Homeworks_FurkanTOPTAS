61b1c69f0bcd9322103dc30a991a2e3672e5b0f4
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-11 22:32:33 +0300

    Create default MessageService

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/Abstract/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.Abstract
+{
+    public interface IMessageService
+    {
+        List<Message> GetAll();
+        Message GetById(int Id);
+        void Add(Message message);
+        void Update(Message message);
+        void Delete(Message message);
+    }
+}
---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/Concretes/MessageService.cs
@@ -0,0 +1,48 @@
+﻿using ApartmanYonetimOtomasyonu.Business.Abstract;
+using ApartmanYonetimOtomasyonu.DataAccess.EntityFramework.Repository.Abstracts;
+using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.Concretes
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IRepository<Message> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public MessageService(IRepository<Message> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Add(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
