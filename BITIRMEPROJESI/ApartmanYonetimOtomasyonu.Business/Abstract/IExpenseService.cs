d43c029535e761069d36aa203a545d06417357f9
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-11 22:31:39 +0300

    Create default ExpenseService

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/Abstract/IExpenseService.cs
@@ -0,0 +1,18 @@
+﻿using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.Abstract
+{
+    public interface IExpenseService
+    {
+        List<Expense> GetAll();
+        Expense GetById(int Id);
+        void Add(Expense expense);
+        void Update(Expense expense);
+        void Delete(Expense expense);        
+    }
+}
---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/Concretes/ExpenseService.cs
@@ -0,0 +1,48 @@
+﻿using ApartmanYonetimOtomasyonu.Business.Abstract;
+using ApartmanYonetimOtomasyonu.DataAccess.EntityFramework.Repository.Abstracts;
+using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.Concretes
+{
+    public class ExpenseService : IExpenseService
+    {
+        private readonly IRepository<Expense> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ExpenseService(IRepository<Expense> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Add(Expense expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Expense expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Expense> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Expense GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Expense expense)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
