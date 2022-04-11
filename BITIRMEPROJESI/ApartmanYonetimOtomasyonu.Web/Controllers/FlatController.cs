0a20dd0eb4e274f0be7548b75ba6da0832037528
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-11 22:36:21 +0300

    Added FlatController

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Controllers/FlatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmanYonetimOtomasyonu.Web.Controllers
+{
+    public class FlatController : Controller
+    {
+        //[AuthorizeByRole(Roles="Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
