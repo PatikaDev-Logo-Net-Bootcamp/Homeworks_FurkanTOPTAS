93351c985085f40219ed733f1413d2b14445d4db
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-11 22:37:16 +0300

    Added TypeOfUser column

--- BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Domain/Entities/User.cs
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Domain/Entities/User.cs
@@ -14,5 +14,6 @@ namespace ApartmanYonetimOtomasyonu.Domain.Entities
         public string TCNo { get; set; }
         public string CarLicensePlate { get; set; }
         public List<Flat> Flats { get; set; }
+        public string TypeOfUser { get; set; } //Kullanıcı Tipi 1- Sahibi 2- Kiracı
     }
 }
