de614f4899eb6254ba61dda383fff1abb75a66e2
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-11 22:32:13 +0300

    Create default ExpenseTypeService

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/Abstract/IExpenseTypeService.cs
@@ -0,0 +1,18 @@
+﻿using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.Abstract
+{
+    public interface IExpenseTypeService
+    {
+        List<ExpenseType> GetAll();
+        ExpenseType GetById(int Id);
+        void Add(ExpenseType expenseType);
+        void Update(ExpenseType expenseType);
+        void Delete(ExpenseType expenseType);
+    }
+}
---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/Concretes/ExpenseTypeService.cs
@@ -0,0 +1,43 @@
+﻿using ApartmanYonetimOtomasyonu.Business.Abstract;
+using ApartmanYonetimOtomasyonu.DataAccess.EntityFramework.Repository.Abstracts;
+using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.Concretes
+{
+    public class ExpenseTypeService : IExpenseTypeService
+    {
+        private readonly IRepository<ExpenseType> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public void Add(ExpenseType expenseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ExpenseType expenseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ExpenseType> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExpenseType GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ExpenseType expenseType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+   
+}
