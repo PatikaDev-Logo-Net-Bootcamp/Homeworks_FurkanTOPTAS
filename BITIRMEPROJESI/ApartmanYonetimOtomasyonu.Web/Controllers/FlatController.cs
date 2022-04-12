4826a419b996baa2e5fda87f675d6bdb9a27e1f7
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-13 00:02:45 +0300

    Added Flat Controller

--- BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Controllers/FlatController.cs
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Controllers/FlatController.cs
@@ -1,13 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApartmanYonetimOtomasyonu.Business.Abstract;
+using ApartmanYonetimOtomasyonu.Business.DTOs;
+using ApartmanYonetimOtomasyonu.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ApartmanYonetimOtomasyonu.Web.Controllers
 {
     public class FlatController : Controller
     {
         //[AuthorizeByRole(Roles="Admin")]
-        public IActionResult Index()
+
+        private readonly IFlatService flatService;
+        private readonly UserManager<User> userManager;
+        private readonly IBuildingService buildingService;
+
+        public FlatController(IFlatService flatService, UserManager<User> userManager, IBuildingService buildingService)
         {
-            return View();
+            this.flatService = flatService;
+            this.userManager = userManager;
+            this.buildingService = buildingService;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() // Daire Listeleme
+        {
+            var flats = await flatService.GetAllFlatsByRelations();
+            return View(flats);           
+        }
+
+        [HttpGet]
+        public IActionResult AddFlat() // Daire Ekleme
+        {
+            var users = userManager.Users.ToList();
+            var building = buildingService.GetAll();
+            var flat = new FlatCreateDto
+            {
+                Buildings = building,
+                Users = users,
+            };
+            return View(flat);
+        }
+        [HttpPost]
+        public IActionResult AddFlat(FlatCreateDto flat) // Daire Ekleme
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(flat);
+            }
+            flatService.Add(new Flat
+            {
+                UserId = flat.UserId,
+                BuildingId = flat.BuildingId,
+                FlatNo = flat.FlatNo,
+                IsEmpty = flat.IsEmpty,
+                IsDeleted = false,
+                Floor = flat.Floor,
+                RoomSize = flat.RoomSize,
+                SaloonSize = flat.SaloonSize,
+            });
+            return RedirectToAction("Index");
+        }
+
     }
 }
