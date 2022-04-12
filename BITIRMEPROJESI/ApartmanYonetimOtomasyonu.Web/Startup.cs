5114a0eaf115be7151c743a165962e83b6cbb9b5
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-13 00:07:08 +0300

    Added  IFlatRepository

--- BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Startup.cs
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Startup.cs
@@ -46,7 +46,8 @@ namespace ApartmanYonetimOtomasyonu.Web
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             services.AddTransient<IBuildingService, BuildingService>();
             services.AddTransient<IFlatService, FlatService>();
-            
+            services.AddScoped<IFlatRepository, FlatRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
