000939037019425fd07774b11923d9a44418203f
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-15 00:35:34 +0300

    Added two diffrent repository(FlatRepository) class but I will there because Not good code design

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.DataAccess.EntityFramework.Repository/Concretes/ExpenseRepository.cs
@@ -0,0 +1,29 @@
+﻿using ApartmanYonetimOtomasyonu.DataAccess.EntityFramework.Repository.Abstracts;
+using ApartmanYonetimOtomasyonu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.DataAccess.EntityFramework.Repository.Concretes
+{
+    public class ExpenseRepository : Repository<Expense> , IExpenseRepository
+    {
+        public ExpenseRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public async Task<List<Expense>> GetAllExpenseByRelationsAsync()
+        {
+
+            return await unitOfWork.Context.Expenses
+                .Include(x => x.ExpenseType)                
+                .Include(x => x.Flat)
+                .ThenInclude(x => x.User)     
+                .Where(x => x.IsDeleted == false)                
+                .ToListAsync();
+        }
+    }
+}
