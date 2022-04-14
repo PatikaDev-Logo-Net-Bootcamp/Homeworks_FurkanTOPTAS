0931bfa4913a3504a540a9effa6a7d7f769309a8
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-15 00:37:20 +0300

    Added Expense DTOs

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/DTOs/ExpenseCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.DTOs
+{
+    public class ExpenseCreateDto
+    {
+        public int Id { get; set; }
+        public bool IsPaid { get; set; }
+        public decimal Price { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public int FlatId { get; set; }
+        public int FlatNo { get; set; }
+        public int ExpenseTypeId { get; set; }
+
+        public IEnumerable<ExpenseType> ExpenseType { get; set; }
+        public IEnumerable<Flat> Flat { get; set; }
+    }
+}
---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/DTOs/ExpenseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.DTOs
+{
+    public class ExpenseDto
+    {
+        public int Id { get; set; }
+        public bool IsPaid { get; set; }
+        public decimal Price { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public int FlatId { get; set; }
+        public int FlatNo { get; set; }
+        public int ExpenseTypeId{ get; set; }
+        public string ExpenseTypeName{ get; set; }
+    }
+}
