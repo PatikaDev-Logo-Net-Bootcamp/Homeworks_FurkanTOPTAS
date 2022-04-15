036d5d287bb3d936bc548e8ad96209820be4bd54
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-15 23:43:18 +0300

    Added Register Page Default User is BasicUser

--- BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -126,6 +126,7 @@ namespace ApartmanYonetimOtomasyonu.Web.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, EntityFramework.Enums.Roles.BasicUser.ToString());
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
