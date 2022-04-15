30224b07cbfa3197b0a121b743a2a63d129a602e
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-16 02:31:16 +0300

    Droped Database and add migration

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.EntityFramework/Migrations/20220415231437_appDbContxtCnfigurationChanged.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApartmanYonetimOtomasyonu.EntityFramework.Migrations
+{
+    public partial class appDbContxtCnfigurationChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flats_User_UserId",
+                schema: "Apartment",
+                table: "Flats");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flats_User_UserId",
+                schema: "Apartment",
+                table: "Flats",
+                column: "UserId",
+                principalSchema: "Apartment",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flats_User_UserId",
+                schema: "Apartment",
+                table: "Flats");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flats_User_UserId",
+                schema: "Apartment",
+                table: "Flats",
+                column: "UserId",
+                principalSchema: "Apartment",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
