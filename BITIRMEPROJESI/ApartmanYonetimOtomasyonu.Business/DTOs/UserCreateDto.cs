1671928567099e89d13f8ce58843bb3f1e506c7c
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-16 23:39:08 +0300

    Added User Dto for Add User Operation

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/DTOs/UserCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.DTOs
+{
+    public class UserCreateDto
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TCNo { get; set; }
+        public string CarLicensePlate { get; set; }
+        public string TypeOfUser { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
