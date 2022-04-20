354046cf607ea582553c8c602b7d2b60661917ac
Furkan TOPTAŞ <48125285+furkantoptass@users.noreply.github.com>
2022-04-20 21:53:30 +0300

    Added Payment Dtos

---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/DTOs/PaymentCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.DTOs
+{
+    public class PaymentCreateDto
+    {
+        public string Owner { get; set; }
+        public string CardNumber { get; set; }
+        public int ValidMonth { get; set; }
+        public int ValidYear { get; set; }
+        public int Cvv { get; set; }
+        public int FlatId { get; set; }
+        public int ExpenseId { get; set; }
+        public decimal InvoiceAmount { get; set; }
+    }
+}
---
+++ BITIRMEPROJESI/ApartmanYonetimOtomasyonu.Business/DTOs/PaymentExpenseDto.cs
@@ -0,0 +1,22 @@
+﻿using ApartmanYonetimOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApartmanYonetimOtomasyonu.Business.DTOs
+{
+    public class PaymentExpenseDto
+    {
+        public int Id { get; set; }
+        public bool IsPaid { get; set; }
+        public decimal Price { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public string UserName { get; set; }
+        public int FlatId { get; set; }
+        public byte FlatNumber { get; set; }
+        public string ExpenseTypeName { get; set; }
+        public ExpenseType ExpenseType { get; set; }
+    }
+}
